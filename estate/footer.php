6b4675e3d12c058d9ad8201cadd7bfc1c5cc24d9
Caleb <bawacaleb14@gmail.com>
2020-07-20 11:46:19 +1200

    Initial commit

---
+++ estate/footer.php
@@ -0,0 +1,65 @@
+ <footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h3>J2 Real Estate.</h3>
+            <p>
+              Hs No. 005 <br>
+              ABC Road, Achimota<br>
+              Ghana <br>
+              P.O.Box AT630, Achimota Market <br><br>
+              <strong>Phone:</strong> +1 5589 55488 55<br>
+              <strong>Email:</strong> info@example.com<br>
+            </p>
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="index.php">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#about">About us</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Our Products</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Building</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Renting & Purchases</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Maintenance</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-newsletter">
+            <h4>Work with us now</h4>
+            <p>.Reliability .Affordability .Quality .Satisfaction </p>
+            <!--<form action="" method="post">
+              <input type="email" name="email"><input type="submit" value="Subscribe">
+            </form>-->
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container d-lg-flex py-4">
+
+      <div class="mr-lg-auto text-center text-lg-left">
+        <div class="copyright">
+          &copy; Copyright <strong><span>J2 Real Estate</span></strong>. All Rights Reserved
+        </div>
+        <div class="credits">
+          Credits <a href="#">BootstrapMade</a>
+        </div>
+      </div>
+      <!-- <div class="social-links text-center text-lg-right pt-3 pt-lg-0">
+        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+      </div> -->
+    </div>
+  </footer>
